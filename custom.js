1ca1b1f3fba95c1dbee8f475e2ca730c526fa6df
ritvikrajpandey <54979992+ritvikrajpandey@users.noreply.github.com>
2020-11-18 17:06:58 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,11 @@
+
+
+	/* Fancybox
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(".fancybox").fancybox({
+		maxWidth: 1200,
+		maxHeight: 600,
+		width: '70%',
+		height: '70%',
+	});
